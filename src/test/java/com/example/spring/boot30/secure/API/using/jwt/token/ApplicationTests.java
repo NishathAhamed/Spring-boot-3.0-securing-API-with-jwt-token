8b1334ac833d2a3a815ea05a88f758df074e0b12
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2023-06-29 19:26:52 +0530

    initial commit

---
+++ src/test/java/com/example/spring/boot30/secure/API/using/jwt/token/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot30.secure.API.using.jwt.token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
