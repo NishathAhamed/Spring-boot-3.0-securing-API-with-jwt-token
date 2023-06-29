56965f408c531b46096da6b3320d0e2cb5c2df0e
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2023-06-29 20:13:31 +0530

    user entity created

---
+++ src/main/java/com/example/spring/boot30/secure/API/using/jwt/token/user/User.java
@@ -0,0 +1,27 @@
+package com.example.spring.boot30.secure.API.using.jwt.token.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+
+}
