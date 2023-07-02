bcfc8bec3a66f0de2bef2f453194a51c2a374cdd
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2023-07-02 12:09:48 +0530

    Implemented methods to generate token and validate token and check the token is expired

--- src/main/java/com/example/spring/boot30/secure/API/using/jwt/token/config/JwtService.java
+++ src/main/java/com/example/spring/boot30/secure/API/using/jwt/token/config/JwtService.java
@@ -2,11 +2,17 @@ package com.example.spring.boot30.secure.API.using.jwt.token.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Service
@@ -33,6 +39,45 @@ public class JwtService {
     private Key getSigningKey() {
         byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    //generate token methods
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
+
+    //is token valid method
+
+    public boolean isTokenValid(String token,UserDetails userDetails){
+        final String username=extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+
+    //is token expired method
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token,Claims::getExpiration);
     }
 
 
